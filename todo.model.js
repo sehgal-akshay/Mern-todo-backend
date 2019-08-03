b30db3f71965050751ff47996335bdf311d828ab
Akshay Sehgal <akshaysehgal@Akshays-MacBook-Pro.local>
2019-08-03 00:29:02 -0400

    Initial commit of backend files

---
+++ todo.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let Todo = new Schema({
+  todo_description: {
+    type: String
+  },
+  todo_responsible: {
+    type: String
+  },
+  todo_priority: {
+    type: String
+  },
+  todo_completed: {
+    type: Boolean
+  },
+  todo_creation: {
+    type: Object
+  },
+  todo_completion: {
+    type: Object
+  }
+});
+
+module.exports = mongoose.model("Todo", Todo);
